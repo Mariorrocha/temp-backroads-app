721f75cb9e2bb5552ca216daac9373fb563d73ce
Mariorrocha <bloodrocha@hotmail.com>
2023-01-25 20:57:17 -0600

    first commit

---
+++ src/components/PageLinks.js
@@ -0,0 +1,9 @@
+import PageLink from './PageLink';
+
+const PageLinks = ({parentClass, itemClass}) => {
+  return (
+    <PageLink parentClass={parentClass} itemClass={itemClass}/>
+  )
+}
+
+export default PageLinks
